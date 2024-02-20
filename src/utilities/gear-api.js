01c2de2c587b65317f7cfd7dc8dd0cdcb0210598
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2024-02-19 16:05:39 -0800

    refactored gear-api module to handle queries

--- src/utilities/gear-api.js
+++ src/utilities/gear-api.js
@@ -2,9 +2,22 @@ import sendRequest from "./send-request";
 
 const BASE_URL = '/api/gear'
 
-export function getProfile() {
-    return sendRequest(BASE_URL)
+export function fetchBrands(query) {
+    const url = `${BASE_URL}/brands?q=${encodeURIComponent(query)}`;
+    return sendRequest(url);
 }
+
+export function fetchModels(query) {
+    const url = `${BASE_URL}/models?q=${encodeURIComponent(query)}`;
+    return sendRequest(url);
+}
+
+export function fetchModifications(query) {
+    const url = `${BASE_URL}/modifications?q=${encodeURIComponent(query)}`;
+    return sendRequest(url);
+}
+
+
 
 export function createProfile() {
     return sendRequest(`${BASE_URL}/create`, 'POST', );
